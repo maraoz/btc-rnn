1968c7050f0e7dafee4d349740dd594da2308ab9
Manuel Araoz <manuelaraoz@gmail.com>
2016-03-06 22:48:31 -0800

    sanitize

--- sanitize.py
+++ sanitize.py
@@ -5,6 +5,40 @@ import csv
 mem_store = Shove()
 root = Shove('file://shovestore')
 
+def parse(filename):
+  n, e = filename.split('.')
+  with open(filename, 'rb') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+      date = row[0]
+      if date == 'Date':
+        names = row
+        continue
 
-print root['2009-04-08']
+      o = root[date] = root.get(date) or {}
+      for index, name in enumerate(names):
+        key = name if index is 0 else (n+'.'+name)
+        o[key] = row[index]
+      print o
+
+
+parse('BAVERAGE-USD.csv')
+parse('BCHAIN-ATRCT.csv')
+parse('BCHAIN-AVBLS.csv')
+parse('BCHAIN-BCDDE.csv')
+parse('BCHAIN-CPTRA.csv')
+parse('BCHAIN-ETRAV.csv')
+parse('BCHAIN-HRATE.csv')
+parse('BCHAIN-NADDU.csv')
+parse('BCHAIN-NTRAN.csv')
+parse('BCHAIN-NTREP.csv')
+parse('BCHAIN-TOUTV.csv')
+parse('BCHAIN-TRFEE.csv')
+parse('BCHAIN-TVTVR.csv')
+
+
+root.sync()
+
+
+  
 
